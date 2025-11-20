87fee13cbe5cff46487e8fc0cd8eac8a3f7cd58b
LeonardoTapia7 <joel.tapia@udla.edu.ec>
2025-11-20 13:52:25 -0500

    funciones.h

--- funciones.h
+++ funciones.h
@@ -1,8 +1,19 @@
+/* Prototipos del proyecto de producción con arreglos*/
 
-void agregarProducto();
-int buscarProducto(char nombre[]);
-void editarProducto();
-void eliminarProducto();
-void calcularProduccion(int tiempoDisponible, int recursosDisponibles);
-int leerEnteroPositivo(const char *mensaje);
-void limpiarSalto(char *cadena);
+int  ingresarProductos(char nombres[][30], float tiempoPorUnidad[], float recursoPorUnidad[],
+int demanda[], int capacidad);
+
+int  buscarProducto(char nombres[][30], int n, char nombre[]);
+
+void mostrarTabla(char nombres[][30], float tiempo[], float recurso[], int demanda[], int n);
+void editarProducto(char nombres[][30], float tiempo[], float recurso[], int demanda[], int n);
+void eliminarProducto(char nombres[][30], float tiempo[], float recurso[], int demanda[], int *n);
+
+float calcularTiempoTotal(float tiempo[], int demanda[], int n);
+float calcularRecursosTotales(float recurso[], int demanda[], int n);
+
+void verificarFactibilidad(float tiempo[], float recurso[], int demanda[], int n);
+
+/* Utilidades para entrada de texto con espacios */
+void limpiarBuffer(void);
+void leerLinea(char destino[], int max);
